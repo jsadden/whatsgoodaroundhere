4c6b066a561c4291aec54f55596d3b55d522c134
Jim <jsadden8@gmail.com>
2020-07-07 04:17:21 -0400

    initial commit

--- server/server.js
+++ server/server.js
@@ -28,6 +28,15 @@ mongoose.connect(config.DATABASE, {
 
 app.use(express.static('client/build'))
 
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req,res)=> {
+        console.log('Works');
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
+
 const port = process.env.PORT || 3001
 
 app.listen(port, ()=> {
